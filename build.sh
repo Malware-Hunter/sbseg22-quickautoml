da6e1df8e5c86e55a6eeb0cb7e3f5fb0eb46af51
estevaog3 <eccosta@protonmail.com>
2022-05-20 14:44:00 -0400

    Update build.sh to install dependencies

--- build.sh
+++ build.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 echo Installing TPOT...
 python3 -m venv env-tpot
-
 PIP=./env-tpot/bin/pip3
-$PIP install setuptools wheel
-$PIP install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu
-# TO DO: instalar demais dependencias
+
+$PIP install -q setuptools wheel
+$PIP install -q torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu
+$PIP install -q numpy scipy scikit-learn pandas joblib 
+$PIP install -q deap update_checker tqdm stopit xgboost
+$PIP install -q tpot
 echo Done
 
-echo Installing autogluon
+echo Installing autogluon...
 python3 -m venv env-autogluon
-
 PIP=./env-autogluon/bin/pip3
-$PIP install setuptools wheel
-# TO DO: instalar demais dependencias
 
+$PIP install -q setuptools wheel
+$PIP install -q torch==1.10.1+cpu -f https://download.pytorch.org/whl/cpu/torch_stable.html
+$PIP install -q autogluon.tabular
 echo Done
 
-echo Installing auto-sklearn
+echo Installing auto-sklearn...
 python3 -m venv env-auto-sklearn
-
 PIP=./env-auto-sklearn/bin/pip3
-$PIP install setuptools wheel
-# TO DO: instalar demais dependencias
 
+$PIP install -q setuptools wheel
+$PIP install -q auto-sklearn
+echo Done
 
