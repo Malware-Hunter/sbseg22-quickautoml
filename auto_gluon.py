94121ceaf27aa34dbedaca147bf4f2ba6aa6284c
Estevão Costa <eccosta@protonmail.com>
2022-05-23 17:49:22 -0400

    Update auto_gluon.py: get dataset from CLI; export result as CSV

--- auto_gluon.py
+++ auto_gluon.py
@@ -1,15 +1,21 @@
-# First install package from terminal:
-# pip install -U pip
-# pip install -U setuptools wheel
-#pip install autogluon 
-#https://auto.gluon.ai/stable/index.html
-
 import sklearn.metrics
 import pandas as pd
 import timeit
 from autogluon.tabular import TabularDataset, TabularPredictor
 from sklearn.model_selection import train_test_split 
-dataset_df = pd.read_csv('DrebinDatasetPermissoes.csv', encoding = 'utf8')
+
+import sys
+from os.path import exists, basename
+
+def parse_dataset():
+    if(not exists(sys.argv[1])):
+        print("Can't find dataset:", sys.argv[1])
+        sys.exit(1)
+    return sys.argv[1]
+dataset_file_path = parse_dataset()
+dataset_name = basename(dataset_file_path)
+
+dataset_df = pd.read_csv(dataset_file_path, encoding = 'utf8')
 df_train,df_test=train_test_split(dataset_df,test_size=0.33,random_state=1)
 test_data=df_test.drop(['class'],axis=1)
 start_time = timeit.default_timer()
@@ -18,4 +24,14 @@ predictor.evaluate(df_test)
 m, s = divmod(timeit.default_timer() - start_time, 60)
 h, m = divmod(m, 60)
 time_str = "%02d:%02d:%02d" % (h, m, s)
-print('Tempo',time_str)
+
+## TO DO: substituir "FALTA CALCULAR" pelo resultado da métrica correspondente
+
+pd.DataFrame({
+    "accuracy": "FALTA CALCULAR",
+    "precision": "FALTA CALCULAR",
+    "recall": "FALTA CALCULAR",
+    "f1_score": "FALTA CALCULAR",
+    "dataset" : dataset_name,
+    "execution_time" : time_str
+}, index=[0]).to_csv(f"result-autogluon-{dataset_name}", index=False)
