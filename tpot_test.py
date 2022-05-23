685df57694257fddf8c3385a178a7803d2d52f74
Estevão Costa <eccosta@protonmail.com>
2022-05-23 18:21:18 -0400

    Update tpot_test.py: get dataset from CLI; export result as CSV

--- tpot_test.py
+++ tpot_test.py
@@ -1,13 +1,21 @@
-# ver instalação
-# http://epistasislab.github.io/tpot/using/
-
 from tpot import TPOTClassifier
 from sklearn.metrics import *
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import timeit
+import sys
+from datetime import datetime
+from os.path import exists, basename
 
-dataset_df = pd.read_csv('DrebinDatasetPermissoes.csv', encoding='utf8')
+def parse_dataset():
+    if(not exists(sys.argv[1])):
+        print("Can't find dataset:", sys.argv[1])
+        sys.exit(1)
+    return sys.argv[1]
+dataset_file_path = parse_dataset()
+dataset_name = basename(dataset_file_path)
+
+dataset_df = pd.read_csv(dataset_file_path, encoding='utf8')
 y = dataset_df['class']
 X = dataset_df.drop(['class'], axis=1)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=1)
@@ -18,11 +26,16 @@ tpot.fit(X_train, y_train)
 print(tpot.score(X_test, y_test))
 tpot.export('tpot_drebin_pipeline.py')
 predictions = tpot.predict(X_test)
+
 m, s = divmod(timeit.default_timer() - start_time, 60)
 h, m = divmod(m, 60)
 time_str = "%02d:%02d:%02d" % (h, m, s)
-print("Accuracy score:", accuracy_score(y_test, predictions))
-print("Precision:", precision_score(y_test, predictions))
-print("Recall:", recall_score(y_test, predictions))
-print("F1_score:", f1_score(y_test, predictions))
-print('Tempo', time_str)
+
+pd.DataFrame({
+    "accuracy": accuracy_score(y_test, predictions),
+    "precision": precision_score(y_test, predictions),
+    "recall": recall_score(y_test, predictions),
+    "f1_score": f1_score(y_test, predictions),
+    "dataset" : dataset_name,
+    "execution_time" : time_str
+}, index=[0]).to_csv(f"result-tpot-{dataset_name}-{datetime.now()}", index=False)
