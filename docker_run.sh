d9e550d970cbbc20ac5686e5a82ce7bc8f8003ee
Diego Kreutz <diego.unipampa@gmail.com>
2022-06-07 11:00:12 -0300

    updates docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -4,6 +4,6 @@
 
 SCRIPT=$1
 shift 
-DATASETS=$(echo $* | sed 's/ /-/g')
+DATASETS=$(echo $* | sed 's/ /-/g;s/\//-/g')
 TS=$(date +%Y%m%d%H%M%S) 
 { time sudo docker run -it automl /bin/bash -c "bash /home/automl/$SCRIPT $*"; } 2> time-$SCRIPT-$DATASETS-$TS.txt 
