684eb22c99e6609b37f94e7b4a52c0529ce96862
estevaog3 <eccosta@protonmail.com>
2022-06-08 17:27:45 -0400

    Update docker_run.sh: print docker container name; remove -v option

--- docker_run.sh
+++ docker_run.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-[[ $1 && $2 && -f $2 && $3 ]] || { echo "Uso: $0 <shared-dir> <run_script.sh> <dataset1> <dataset2> ... <datasetN>
+[[ $1 && -f $1 && $2 ]] || { echo "Uso: $0 <run_script.sh> <dataset1> <dataset2> ... <datasetN>
 
-    exemplo: $0 /home/<user>/shared-dir run_quick.sh datasets/androcrawl_limpo.csv">&2; exit 1; }
-
-SHARED_DIR=$1
-[[ -d $SHARED_DIR && `echo $SHARED_DIR | grep -o '^/'` == "/"  ]] || { echo "$SHARED_DIR deve ser um caminho absoluto para um diretório existente"; exit 1; }
-shift
+    exemplo: $0 run_quick.sh datasets/androcrawl_limpo.csv">&2; exit 1; }
 
 SCRIPT=$1
 shift 
 DATASETS=$(echo $* | sed 's/ /-/g;s/\//-/g')
 TS=$(date +%Y%m%d%H%M%S)
 CONTAINER_DIR="/home/automl"
-{ time sudo docker run -it -v $SHARED_DIR:$CONTAINER_DIR automl /bin/bash -c "bash $CONTAINER_DIR/$SCRIPT $*"; } 2> time-$SCRIPT-$DATASETS-$TS.txt
+CONTAINER_NAME="container-$SCRIPT-$DATASETS-$TS"
+echo "Nome do container: $CONTAINER_NAME"
+{ time sudo docker run --name $CONTAINER_NAME -it automl /bin/bash -c "bash $CONTAINER_DIR/$SCRIPT $*"; } 2> time-$SCRIPT-$DATASETS-$TS.txt
