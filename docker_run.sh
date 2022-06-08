22e8d1baed737d012182d038b802df25a525b20f
estevaog3 <eccosta@protonmail.com>
2022-06-08 13:38:15 -0400

    Update docker_run.sh: use directory instead of volume

--- docker_run.sh
+++ docker_run.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-[[ $1 && $2 && -f $2 && $3 && -f $3 ]] || { echo "Uso: $0 <volume> <run_script.sh> <dataset1> <dataset2> ... <datasetN>
+[[ $1 && $2 && -f $2 && $3 && -f $3 ]] || { echo "Uso: $0 <shared-dir> <run_script.sh> <dataset1> <dataset2> ... <datasetN>
 
-    exemplo: $0 shared_volume run_quick.sh datasets/androcrawl_limpo.csv">&2; exit 1; }
+    exemplo: $0 /home/<user>/shared-dir run_quick.sh datasets/androcrawl_limpo.csv">&2; exit 1; }
 
-VOLUME=$1
-VOLUME_PATH=`docker volume inspect $VOLUME | awk '/Mountpoint/ {print $2}' | sed 's/[",]//g'`
-[[ $VOLUME_PATH != "" ]] || { echo "Crie o volume '$VOLUME' primeiro: docker volume create $VOLUME"; exit 1; }
+SHARED_DIR=$1
+[[ -d $SHARED_DIR && `echo $SHARED_DIR | grep -o '^/'` == "/"  ]] || { echo "$SHARED_DIR deve ser um caminho absoluto para um diretório existente"; exit 1; }
 shift
 
 SCRIPT=$1
@@ -14,7 +13,4 @@ shift
 DATASETS=$(echo $* | sed 's/ /-/g;s/\//-/g')
 TS=$(date +%Y%m%d%H%M%S)
 CONTAINER_DIR="/home/automl"
-{ time sudo docker run -it -v $VOLUME:$CONTAINER_DIR automl /bin/bash -c "bash $CONTAINER_DIR/$SCRIPT $*"; } 2> time-$SCRIPT-$DATASETS-$TS.txt
-
-echo "Diretório local que é compartilhado com o container:
-$VOLUME_PATH"
+{ time sudo docker run -it -v $SHARED_DIR:$CONTAINER_DIR automl /bin/bash -c "bash $CONTAINER_DIR/$SCRIPT $*"; } 2> time-$SCRIPT-$DATASETS-$TS.txt
