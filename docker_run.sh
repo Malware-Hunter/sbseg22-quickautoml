26f3e208f83e3a21f07b8e5a37dbb86b73219095
estevaog3 <eccosta@protonmail.com>
2022-06-08 12:51:19 -0400

    Update docker_run.sh: use volume from argument

--- docker_run.sh
+++ docker_run.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-[[ $1 ]] && [[ -f $1 ]] && [[ $2 ]] && [[ -f $2 ]] || { echo "Uso: $0 <run_script.sh> <dataset1> <dataset2> ... <datasetN>"; exit; }
+[[ $1 && $2 && -f $2 && $3 && -f $3 ]] || { echo "Uso: $0 <volume> <run_script.sh> <dataset1> <dataset2> ... <datasetN>
+
+    exemplo: $0 shared_volume run_quick.sh datasets/androcrawl_limpo.csv">&2; exit 1; }
+
+VOLUME=$1
+VOLUME_PATH=`docker volume inspect $VOLUME | awk '/Mountpoint/ {print $2}' | sed 's/[",]//g'`
+[[ $VOLUME_PATH != "" ]] || { echo "Crie o volume '$VOLUME' primeiro: docker volume create $VOLUME"; exit 1; }
+shift
 
 SCRIPT=$1
 shift 
 DATASETS=$(echo $* | sed 's/ /-/g;s/\//-/g')
-TS=$(date +%Y%m%d%H%M%S) 
-{ time sudo docker run -it automl /bin/bash -c "bash /home/automl/$SCRIPT $*"; } 2> time-$SCRIPT-$DATASETS-$TS.txt 
+TS=$(date +%Y%m%d%H%M%S)
+CONTAINER_DIR="/home/automl"
+{ time sudo docker run -it -v $VOLUME:$CONTAINER_DIR automl /bin/bash -c "bash $CONTAINER_DIR/$SCRIPT $*"; } 2> time-$SCRIPT-$DATASETS-$TS.txt
+
+echo "Diretório local que é compartilhado com o container:
+$VOLUME_PATH"
