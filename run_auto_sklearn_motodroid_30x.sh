733b795a27638ee2170825dc6ab2015a95155978
Diego Kreutz <diego.unipampa@gmail.com>
2022-06-18 15:38:32 -0300

    run_auto_sklearn_motodroid_30x.sh

--- run_auto_sklearn_motodroid_30x.sh
+++ run_auto_sklearn_motodroid_30x.sh
@@ -16,7 +16,7 @@ cd ..
 for i in `seq 1 30`
 do
 	echo "********* exec $i de 30 *********"
-	for DATASET in datasets/[a-d]*.csv
+	for DATASET in datasets/m*.csv
 	do
 		echo "Begin tests with dataset $DATASET"
 		$AUTO_SKLEARN_PYTHON auto_sklearn.py $DATASET
